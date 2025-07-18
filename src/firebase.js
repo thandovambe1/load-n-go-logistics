9c5e073a488a32ade0469235cb884838dcb4c5e5
thandovambe1 <thandovambe@gmail.com>
2025-07-18 09:50:53 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,21 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDspiXD9hUiHRDOVLhnNoObl7MB_xBoLtk",
+  authDomain: "loand-n-go-logistics.firebaseapp.com",
+  projectId: "loand-n-go-logistics",
+  storageBucket: "loand-n-go-logistics.firebasestorage.app",
+  messagingSenderId: "597245927020",
+  appId: "1:597245927020:web:b42d81d58e363433578249",
+  measurementId: "G-NL5KWT8ZPB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
 
