25b017b7698ff08c6b84856049fa7f7a751b0c8b
thandovambe1 <thandovambe@gmail.com>
2025-07-18 14:49:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import WhatsAppButton from "./components/WhatsAppButton";
 import Home from "./pages/Home";
-import MyBookings from "./pages/MyBookings";
+import Bookings from "./pages/Bookings";
 import Contact from "./pages/Contact";
 import Profile from "./pages/Profile";
+import WhatsAppButton from "./components/WhatsAppButton";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100">
+      <div className="app">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/bookings" element={<MyBookings />} />
+          <Route path="/bookings" element={<Bookings />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
