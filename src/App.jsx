b9636c692cb2053f8d4ea217467f50c164d490bc
thandovambe1 <thandovambe@gmail.com>
2025-07-20 21:46:01 +0200

    App.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Bookings from "./pages/Bookings";
-import Contact from "./pages/Contact";
-import Profile from "./pages/Profile";
-import WhatsAppButton from "./components/WhatsAppButton";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
-function App() {
-  return (
-    <Router>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/bookings" element={<Bookings />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-        <WhatsAppButton />
-      </div>
-    </Router>
-  );
-}
-
-export default App;
+// Inside App component:
+<Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/bookings" element={<Bookings />} />
+  <Route path="/profile" element={<Profile />} />
+  <Route path="/contact" element={<Contact />} />
+  <Route path="/login" element={<Login />} />
+</Routes>
