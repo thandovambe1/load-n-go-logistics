dc45f3a607cd1981ea16859dea4d562bf991330b
thandovambe1 <thandovambe@gmail.com>
2025-07-20 21:36:40 +0200

    Firebase edit

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,20 @@
+// Firebase configuration and initialization
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDspiXD9hUiHRDOVLhnNoObl7MB_xBoLtk",
-  authDomain: "loand-n-go-logistics.firebaseapp.com",
-  projectId: "loand-n-go-logistics",
-  storageBucket: "loand-n-go-logistics.firebasestorage.app",
+  authDomain: "load-n-go-logistics.firebaseapp.com",
+  projectId: "load-n-go-logistics",
+  storageBucket: "load-n-go-logistics.firebasestorage.app",
   messagingSenderId: "597245927020",
   appId: "1:597245927020:web:b42d81d58e363433578249",
-  measurementId: "G-NL5KWT8ZPB"
+  measurementId: "G-NL5KWT8ZPB",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Auth and Google provider
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
