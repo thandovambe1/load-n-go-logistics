22219b0625e2bf639e172dfaca97012d26a54d22
thandovambe1 <thandovambe@gmail.com>
2025-07-22 00:34:01 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,12 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDspiXD9hUiHRDOVLhnNoObl7MB_xBoLtk",
   authDomain: "load-n-go-logistics.firebaseapp.com",
   projectId: "load-n-go-logistics",
-  storageBucket: "load-n-go-logistics.appspot.com",
+  storageBucket: "load-n-go-logistics.firebasestorage.app",
   messagingSenderId: "597245927020",
   appId: "1:597245927020:web:b42d81d58e363433578249",
   measurementId: "G-NL5KWT8ZPB"
@@ -16,4 +15,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
