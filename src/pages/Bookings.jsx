71be298e129b15db1b5c2ac688b8e3d0fb644194
thandovambe1 <thandovambe@gmail.com>
2025-07-21 19:25:02 +0200

    Delete src directory

--- src/pages/Bookings.jsx
+++
@@ -1,8 +0,0 @@
-export default function MyBookings() {
-  return (
-    <div className="p-6">
-      <h2 className="text-3xl font-bold mb-4">My Bookings</h2>
-      <p className="text-gray-600">No bookings yet. Start by making your first booking!</p>
-    </div>
-  );
-}
