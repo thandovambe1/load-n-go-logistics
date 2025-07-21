afd6f0c79bca4429c7930ad4d467a18d7cb9ec0a
thandovambe1 <thandovambe@gmail.com>
2025-07-21 20:14:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import MyBookings from "./pages/MyBookings";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/bookings" element={<MyBookings />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-        <a href="https://wa.me/your-number" 
-           className="fixed bottom-5 right-5 bg-green-500 text-white px-4 py-3 rounded-full shadow-lg">
-          WhatsApp Us
-        </a>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
+
 export default App;
