517d44e175aa5b6b51027234aa1d4a49a9b41ef1
thandovambe1 <thandovambe@gmail.com>
2025-07-21 11:06:48 +0200

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Components
 import Navbar from "./components/Navbar";
-
-// Pages
 import Home from "./pages/Home";
 import Bookings from "./pages/Bookings";
-import Profile from "./pages/Profile";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <div className="font-sans bg-gray-50 min-h-screen">
-        {/* Navbar on top */}
-        <Navbar />
-
-        {/* Page Content */}
-        <div className="pt-24 px-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/bookings" element={<Bookings />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bookings" element={<Bookings />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </Router>
   );
-};
+}
 
 export default App;
