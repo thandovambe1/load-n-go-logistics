b2c2298fb58ac6307bee7a9fb93aa167a27763f4
thandovambe1 <thandovambe@gmail.com>
2025-07-21 23:19:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import MyBookings from "./pages/MyBookings";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/bookings" element={<MyBookings />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
