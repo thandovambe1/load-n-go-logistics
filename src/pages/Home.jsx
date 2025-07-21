3493b9b23ee812e19b20c340d657ad67a7581589
thandovambe1 <thandovambe@gmail.com>
2025-07-21 19:30:04 +0200

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center text-center p-8">
+      <h1 className="text-4xl font-bold text-blue-900">Welcome to Load-N-Go Logistics</h1>
+      <p className="mt-4 text-gray-600">Book your delivery easily and securely.</p>
+    </div>
+  );
+}
