19a04986f95550e18d09ce20dd63c710f109dd04
thandovambe1 <thandovambe@gmail.com>
2025-07-21 23:50:02 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import WhatsAppButton from "./components/WhatsAppButton";
 import Home from "./pages/Home";
 import MyBookings from "./pages/MyBookings";
-import Profile from "./pages/Profile";
-import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import WhatsAppButton from "./components/WhatsAppButton";
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/my-bookings" element={<MyBookings />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <WhatsAppButton />
-      <Footer />
+      <div className="min-h-screen bg-gray-100">
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Main Content */}
+        <div className="container mx-auto px-4 py-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/my-bookings" element={<MyBookings />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+
+        {/* WhatsApp Floating Button */}
+        <WhatsAppButton />
+      </div>
     </Router>
   );
 }
