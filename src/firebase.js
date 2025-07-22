37f5867358f3d73aea4c6f83fe59eb42543e6b9c
thandovambe1 <thandovambe@gmail.com>
2025-07-22 23:19:50 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,22 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDspiXD9hUiHRDOVLhnNoObl7MB_xBoLtk",
   authDomain: "loand-n-go-logistics.firebaseapp.com",
   projectId: "loand-n-go-logistics",
-  storageBucket: "loand-n-go-logistics.appspot.com",
+  storageBucket: "loand-n-go-logistics.firebasestorage.app",
   messagingSenderId: "597245927020",
   appId: "1:597245927020:web:b42d81d58e363433578249",
   measurementId: "G-NL5KWT8ZPB"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
 
-// ✅ Export all services
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export { db, auth, storage };
