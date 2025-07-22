98aad6f07aef92b02b21b0d5df6a66521a351cbe
thandovambe1 <thandovambe@gmail.com>
2025-07-22 23:27:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ Ensures correct asset paths on Vercel
+  base: "./", // ✅ Fix asset path for Vercel
 });
