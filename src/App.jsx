c131f54e13c0d612437aa97490b36ae5a325c119
thandovambe1 <thandovambe@gmail.com>
2025-07-22 22:59:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import PartnerRegister from "./pages/PartnerRegister";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
-export default function App() {
+function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/partner-register" element={<PartnerRegister />} />
-          </Routes>
-        </main>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/partner-register" element={<PartnerRegister />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </Router>
   );
 }
+
+export default App;
