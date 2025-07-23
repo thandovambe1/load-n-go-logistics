db18f664334f66529d55a5495b333bf59849fff4
thandovambe1 <thandovambe@gmail.com>
2025-07-23 17:18:30 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import PartnerRegister from "./pages/PartnerRegister";
-import { AuthProvider } from "./context/AuthContext";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/partner-register" element={<PartnerRegister />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/services" element={<div>Services Page</div>} />
+        <Route path="/bookings" element={<div>My Bookings</div>} />
+        <Route path="/partner-register" element={<div>Partner Register</div>} />
+      </Routes>
+    </Router>
   );
 };
 
