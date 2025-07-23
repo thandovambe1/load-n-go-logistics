808f72c5e66da1d4682927ec039b529e603285da
thandovambe1 <thandovambe@gmail.com>
2025-07-23 20:22:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+import Hero from "./components/Hero";
 import Services from "./pages/Services";
-import MyBookings from "./pages/MyBookings";
-import PartnerRegister from "./pages/PartnerRegister";
-import ContactSupport from "./pages/ContactSupport";
+import Contact from "./pages/Contact";
+import TrackDriver from "./pages/TrackDriver";
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/bookings" element={<MyBookings />} />
-        <Route path="/partner-register" element={<PartnerRegister />} />
-        <Route path="/contact-support" element={<ContactSupport />} />
-      </Routes>
+      <div className="font-sans">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/track-driver" element={<TrackDriver />} />
+        </Routes>
+      </div>
     </Router>
   );
-};
+}
 
 export default App;
