96cbc6b2d856934e70ad21e7ff76128dd3d743f8
thandovambe1 <thandovambe@gmail.com>
2025-07-24 21:13:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Booking from "./components/Booking";
 
 function App() {
   return (
     <div>
       <Hero />
-      {/* Other sections like Services, Contact, etc. */}
+      <Services />
+      <Booking />
     </div>
   );
 }
