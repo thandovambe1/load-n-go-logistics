26c79d2f0d760838aab9a4e9fabe2e3ef76228a5
thandovambe1 <thandovambe@gmail.com>
2025-07-28 19:00:12 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -74,3 +74,5 @@ const sendEmail = (e) => {
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
               transition={{ duration: 0.5, delay: i
+                );
+};  
