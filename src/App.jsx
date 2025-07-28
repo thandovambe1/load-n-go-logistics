7146d66d4a5823459be687f8ee3016ac12a2e1a7
thandovambe1 <thandovambe@gmail.com>
2025-07-28 14:58:17 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import MyBookings from "./pages/MyBookings";
+import ContactSupport from "./pages/ContactSupport";
+
+<Route path="/contact-support" element={<ContactSupport />} />
 
 function App() {
   return (
