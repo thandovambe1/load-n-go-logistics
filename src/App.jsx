6504e1a982a072dfe9f43ba9353ecc77117443de
thandovambe1 <thandovambe@gmail.com>
2025-07-29 09:58:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Booking from './pages/Booking';
+import BookNow from './pages/BookNow';
 import Dashboard from './pages/Dashboard';
-// other imports...
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/book" element={<Booking />} />
+        <Route path="/book" element={<BookNow />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* Add other routes here */}
       </Routes>
     </Router>
   );
